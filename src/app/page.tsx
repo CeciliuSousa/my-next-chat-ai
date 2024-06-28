ea9a94cef1cf049fdd2732f12b25b15253b51088
Cecilius Sousa <ceciliusousa@gmail.com>
2024-06-28 00:00:10 -0300

    Resolvendo erro de response, falta de cota openai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
+import { Chat } from '@/components/Chat';
 
 export default function Home() {
   return (
     <div className='flex min-h-screen bg-slate-200 items-center justify-center'>
-      <Card className='w-[400px] h-[500px] grid grid-rows-[min-content_1fr_min-content] shadow-md shadow-slate-800 '>
-        <CardHeader>Chat Bot</CardHeader>
-        <CardDescription>Chat bot with varcel.</CardDescription>
-
-        <CardContent className='space-y-3'>
-          <div>
-            <Avatar>
-              <AvatarFallback>CS</AvatarFallback>
-              <AvatarImage src='http://github.com/CeciliuSousa.png'></AvatarImage>
-            </Avatar>
-          </div>
-        </CardContent>
-
-        <CardFooter className='space-y-3'>
-          <Input placeholder='Em que posso lhe ajudar?'></Input>
-          <Button type='submit'>Send</Button>
-        </CardFooter>
-      </Card>
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button } from './ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Input } from './ui/input';
+import { ScrollArea } from './ui/scroll-area';
+
+export function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <Card className='w-[550px]'>
+      <CardHeader>
+        <CardTitle>Chat Bot</CardTitle>
+        <CardDescription>Chat bot with varcel.</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className='h-[400px] w-full space-y-4 '>
+          {messages.map((message) => {
+            return (
+              <div
+                key={message.id}
+                className='flex gap-3 text-slate-600 text-sm '
+              >
+                {message.role === 'user' && (
+                  <Avatar>
+                    <AvatarFallback>CS</AvatarFallback>
+                    <AvatarImage src='http://github.com/CeciliuSousa.png'></AvatarImage>
+                  </Avatar>
+                )}
+                {message.role === 'assistant' && (
+                  <Avatar>
+                    <AvatarFallback>AI</AvatarFallback>
+                    <AvatarImage src='https://www.shutterstock.com/image-vector/chat-bot-logo-design-concept-600nw-1938811039.jpg'></AvatarImage>
+                  </Avatar>
+                )}
+
+                <p className='leading-relaxed'>
+                  <span className='block font-bold text-slate-700'>
+                    {message.role === 'user' ? 'You' : 'AI'}:
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </CardContent>
+
+      <CardFooter>
+        <form className='w-full flex gap-3' onSubmit={handleSubmit}>
+          <Input
+            placeholder='Em que posso lhe ajudar?'
+            value={input}
+            onChange={handleInputChange}
+          ></Input>
+          <Button type='submit'>Send</Button>
+        </form>
+      </CardFooter>
+    </Card>
+  );
+}
